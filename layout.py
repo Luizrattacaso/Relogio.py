a4b7e9d0b399f8d27c212de51bc86f57f5dfe1e4
Luizrattacaso <rattacasoluiz03@gmail.com>
2025-03-24 20:29:45 -0300

    Update layout.py

--- layout.py
+++ layout.py
@@ -3,24 +3,21 @@ import time
 
 # Função que atualiza o relógio
 def atualizar_relogio():
-    hora_atual = time.strftime(f'%H:%M:%S')  # Obtém a hora atual no formato HH:MM:SS
-    label_relogio.configure(text=hora_atual)  # Atualiza o texto do label
-    # Chama a função novamente a cada 1000ms (1 segundo)
+    hora_atual = time.strftime(f'%H:%M:%S')
+    label_relogio.configure(text=hora_atual)
     janela.after(1000, atualizar_relogio)
 
 # Criação da janela principal
 janela = ctk.CTk()
 janela.title("Relógio Digital")
-janela.geometry("300x150")
+janela.geometry("400x250")
 janela.configure(bg='black')
 janela._set_appearance_mode("dark")
 
 # Criando o label do relógio
 label_relogio = ctk.CTkLabel(janela, text="", font=("Arial", 90), text_color="green", fg_color="black")
-label_relogio.pack(pady=50)  # Adiciona o label à janela e centraliza
+label_relogio.pack(pady=50)
 
-# Inicia a atualização do relógio
 atualizar_relogio()
 
-# Executa a aplicação
 janela.mainloop()
